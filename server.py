3392ecdb92583dac72cd8f2faa002a8161a0e95b
imeliorut <50800750+imeliorut@users.noreply.github.com>
2023-05-14 18:55:20 -0400

    Update server.py

--- server.py
+++ server.py
@@ -35,6 +35,6 @@ def allowed_file(filename):
 
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-app.run(host='0.0.0.0', port=8080, debug=True)
+app.run(host='0.0.0.0', port=80)
 
 # TODO: Add conic equations
